1dbf083528b87aea3708a5cb7f8872ffd224298e
juztinlazar <juztinlazar@gmail.com>
2019-10-09 21:20:44 +0800

    changes: schmea validation

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,45 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { IDataProps } from './library/interface';
+import onValidateRequired from './library/validateRequiredData';
+import onValidateSchema from './library/validateSchema';
+import onValidateDataType from './library/validateDataType';
+
+
+const schema: IDataProps[] = [
+  {
+    fieldName: 'todo_id',
+    fieldType: 'string'
+  },
+  {
+    fieldName: 'first_name',
+    fieldType: 'string'
+  },
+  {
+    fieldName: 'age',
+    fieldType: 'string'
+  }
+];
+
+const data = {
+  todo_id: 1,
+  last_name: '321312'
+}
+
 
 const App: React.FC = () => {
+  const isValidRequired = onValidateRequired(schema, data);
+  console.log('isValidRequired', isValidRequired)
+
+  const returnOnlySchema = onValidateSchema(schema, data);
+  console.log('returnOnlySchema', returnOnlySchema)
+
+  const validateDataTypes = onValidateDataType(schema, data)
+  console.log('validateDataTypes', validateDataTypes)
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>Schema-validation</h2>
     </div>
   );
 }
